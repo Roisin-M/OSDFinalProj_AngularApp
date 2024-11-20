b1ad59aaf269f729a1cdde59b389124aa9fdd4b9
roisin-m <s00223865@atu.ie>
2024-11-20 20:16:27 +0000

    set up connection with service, component and interface for class Location

---
+++ src/app/components/class-location/class-location.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ClassLocationsService } from '../../services/classLocations/class-locations.service';
+import { ClassLocation } from '../../interfaces/class-location';
+
+@Component({
+  selector: 'app-class-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './class-location.component.html',
+  styleUrl: './class-location.component.css'
+})
+export class ClassLocationComponent {
+  classLocations : ClassLocation[]=[];
+  message: string='';
+
+  //constuctor with injection
+  constructor(private classLocationService: ClassLocationsService){}
+  //ngonit method
+  ngOnInit():void{
+    this.classLocationService.getClassLocations().subscribe({
+      next: (value: ClassLocation[])=>{
+        console.log('here');
+        this.classLocations = value},
+        complete:()=> console.log('class Location service finished'),
+        error: (message)=>this.message=message
+    })
+  }
+}
