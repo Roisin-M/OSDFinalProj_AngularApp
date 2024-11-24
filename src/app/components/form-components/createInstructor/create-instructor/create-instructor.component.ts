302c888a6c1acd2634fc526c97cc6ef1bc864828
roisin-m <s00223865@atu.ie>
2024-11-24 20:28:27 +0000

    material form for create instructor, no error validation yet, but material working

--- src/app/components/form-components/createInstructor/create-instructor/create-instructor.component.ts
+++ src/app/components/form-components/createInstructor/create-instructor/create-instructor.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
 import { YogaSpeciality } from '../../../../interfaces/instructor';
-
+//material imports
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import{MatRadioButton, MatRadioModule} from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-create-instructor',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, MatButtonModule,MatFormFieldModule, MatInputModule, MatCardModule, 
+    MatRadioModule, MatSelectModule],
   templateUrl: './create-instructor.component.html',
   styleUrl: './create-instructor.component.css'
 })
@@ -19,19 +26,27 @@ export class CreateInstructorComponent {
     this.createInstructorForm = this.formBuilder.group({
       name: new FormControl(''),
       email: new FormControl(''),
-      yogaSpecialities: this.formBuilder.array([]),
+      yogaSpecialities: this.formBuilder.array([])
     });
   }
     //getter to get the formArray
-    get yogaSpecialitiesArray():FormArray{
-      return this.createInstructorForm.get('yogaSpecialities') as FormArray;
+    // get yogaSpecialitiesArray():FormArray{
+    //   return this.createInstructorForm.get('yogaSpecialities') as FormArray;
+    // }
+    // explicitly casts the controls to FormControl[]
+    get yogaSpecialitiesArray(): FormArray<FormControl> {
+      return this.createInstructorForm.get('yogaSpecialities') as FormArray<FormControl>;
     }
-
+    
     //add a Yoga Speciality
-    addYogaSpeciality(): void{
-      const specialityControl = this.formBuilder.control('');
-      this.yogaSpecialitiesArray.push(specialityControl);
+    // addYogaSpeciality(): void{
+    //   const specialityControl = this.formBuilder.control('');
+    //   this.yogaSpecialitiesArray.push(specialityControl);
+    // }
+    addYogaSpeciality(): void {
+      this.yogaSpecialitiesArray.push(this.formBuilder.control('')); // Adds a FormControl explicitly
     }
+    
 
     //remove a speciality
     removeYogaSpeciality(index: number):void{
