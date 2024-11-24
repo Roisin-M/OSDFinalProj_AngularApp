95a1a4e016e39bb636711aa090b78be22aac2fb4
roisin-m <s00223865@atu.ie>
2024-11-24 22:09:06 +0000

    validators and errors for instructor create form

--- src/app/components/form-components/createInstructor/create-instructor/create-instructor.component.ts
+++ src/app/components/form-components/createInstructor/create-instructor/create-instructor.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, FormGroup, 
+  FormControl, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { YogaSpeciality } from '../../../../interfaces/instructor';
 //material imports
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
-import{MatRadioButton, MatRadioModule} from '@angular/material/radio';
+import{MatRadioModule} from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-create-instructor',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatButtonModule,MatFormFieldModule, MatInputModule, MatCardModule, 
+  imports: [FormsModule, ReactiveFormsModule, MatButtonModule,
+    MatFormFieldModule, MatInputModule, MatCardModule, 
     MatRadioModule, MatSelectModule],
   templateUrl: './create-instructor.component.html',
   styleUrl: './create-instructor.component.css'
@@ -24,29 +26,33 @@ export class CreateInstructorComponent {
   constructor(private formBuilder: FormBuilder){
     //initialise the form in the constructor
     this.createInstructorForm = this.formBuilder.group({
-      name: new FormControl(''),
-      email: new FormControl(''),
-      yogaSpecialities: this.formBuilder.array([])
+      name: new FormControl('',[Validators.required,
+        Validators.minLength(3)]),
+      email: new FormControl('', [Validators.email, 
+        Validators.required]),
+      yogaSpecialities: this.formBuilder.array([], Validators.required)
     });
   }
-    //getter to get the formArray
-    // get yogaSpecialitiesArray():FormArray{
-    //   return this.createInstructorForm.get('yogaSpecialities') as FormArray;
-    // }
+ 
     // explicitly casts the controls to FormControl[]
     get yogaSpecialitiesArray(): FormArray<FormControl> {
       return this.createInstructorForm.get('yogaSpecialities') as FormArray<FormControl>;
     }
-    
+
     //add a Yoga Speciality
-    // addYogaSpeciality(): void{
-    //   const specialityControl = this.formBuilder.control('');
-    //   this.yogaSpecialitiesArray.push(specialityControl);
-    // }
-    addYogaSpeciality(): void {
-      this.yogaSpecialitiesArray.push(this.formBuilder.control('')); // Adds a FormControl explicitly
+    addYogaSpeciality(): void{
+      const specialityControl = this.formBuilder.control('', Validators.required);
+      this.yogaSpecialitiesArray.push(specialityControl);
+    }
+  
+    //getters for validation
+    get name() {
+      return this.createInstructorForm.get('name');
     }
     
+    get email() {
+      return this.createInstructorForm.get('email');
+    }
 
     //remove a speciality
     removeYogaSpeciality(index: number):void{
--- src/app/components/form-components/createclasslocation/create-class-location/create-class-location.component.ts
+++ src/app/components/form-components/createclasslocation/create-class-location/create-class-location.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
 import { ClassFormat } from '../../../../interfaces/class-location';
+//material imports
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import{MatRadioModule} from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-create-class-location',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, MatButtonModule,
+    MatFormFieldModule, MatInputModule, MatCardModule, 
+    MatRadioModule, MatSelectModule],
   templateUrl: './create-class-location.component.html',
   styleUrl: './create-class-location.component.css'
 })
@@ -23,12 +32,12 @@ export class CreateClassLocationComponent {
         classFormats: this.formBuilder.array([]),
     });
   }
-  //getter to get the formArray
-  get classFormatsArray():FormArray{
-    return this.createClassLocationForm.get('classFormats') as FormArray;
+  //getter to get the formArray -> explicity cast as type FormControl
+  get classFormatsArray():FormArray<FormControl>{
+    return this.createClassLocationForm.get('classFormats') as FormArray<FormControl>;
   }
   
-  //add a Class Format
+  //add a Class Format as form control
   addClassFormat():void{
     const formatControl = this.formBuilder.control('');
     this.classFormatsArray.push(formatControl);
