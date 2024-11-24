fbc696643f8233a68761d1e7eeb5281b6345e0b3
roisin-m <s00223865@atu.ie>
2024-11-24 14:10:13 +0000

    imported bootstrap and made a navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { InstructorComponent } from "./components/instructor/instructor.component";
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InstructorComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
