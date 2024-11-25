875063424b4109219a4d2d8bb12c126418d5a929
roisin-m <s00223865@atu.ie>
2024-11-25 13:35:36 +0000

    initial commit

---
+++ src/app/components/view-details-components/class-locationDetails/class-location-details/class-location-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-location-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './class-location-details.component.html',
+  styleUrl: './class-location-details.component.css'
+})
+export class ClassLocationDetailsComponent {
+
+}
---
+++ src/app/components/view-details-components/instructorDetails/instructor-details/instructor-details.component.ts
@@ -0,0 +1,58 @@
+import { Component,inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Instructor } from '../../../../interfaces/instructor';
+import { InstructorsService } from '../../../../services/instructors/instructors.service';
+import { AsyncPipe, DecimalPipe, TitleCasePipe } from '@angular/common';
+import { CreateInstructorComponent } from '../../../form-components/createInstructor/create-instructor/create-instructor.component';
+import { MatCardModule } from '@angular/material/card'
+import { MatButton, MatButtonModule } from '@angular/material/button'
+
+@Component({
+  selector: 'app-instructor-details',
+  standalone: true,
+  imports: [AsyncPipe, CreateInstructorComponent, 
+    MatCardModule, DecimalPipe, TitleCasePipe, MatButton],
+  templateUrl: './instructor-details.component.html',
+  styleUrl: './instructor-details.component.css'
+})
+export class InstructorDetailsComponent {
+
+  id:string | null="";
+  showForm: boolean = false;
+
+  instructor$ : Observable<Instructor> | undefined
+
+  constructor(private route: ActivatedRoute,
+    private instructorsService: InstructorsService,
+    private router:Router
+  ){}
+
+  //get by id from service
+  ngOnInit():void{
+    this.id=this.route.snapshot.paramMap.get('id');
+    if(this.id){
+      this.instructor$ = this.instructorsService.getInstructor(this.id)
+    }
+  }
+
+  editInstructor():void{
+    this.showForm=true;
+  }
+
+  deleteInstructor():void{
+    console.log("in delete");
+
+    if(this.id){
+      this.instructorsService.deleteInstructor(this.id)
+      .subscribe({
+        next: response=>{
+          this.router.navigateByUrl('/instructors')},
+          error:(err:Error)=>{
+            console.log(err.message);
+            //this.message = err
+          }
+      })
+    }
+  }
+}
