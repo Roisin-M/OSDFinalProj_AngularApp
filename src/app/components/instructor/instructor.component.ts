7ef30dba3fc12b78202fcbecf5ce13507a939e0f
roisin-m <s00223865@atu.ie>
2024-12-20 14:05:18 +0000

    filter instructors by yoga speciality

--- src/app/components/instructor/instructor.component.ts
+++ src/app/components/instructor/instructor.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { InstructorsService } from '../../services/instructors/instructors.service';
-import { Instructor } from '../../interfaces/instructor';
+import { Instructor, YogaSpeciality } from '../../interfaces/instructor';
 import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-instructor',
   standalone: true,
-  imports: [RouterLink, MatCardModule],
+  imports: [RouterLink, MatCardModule, MatFormFieldModule, MatSelectModule, FormsModule],
   templateUrl: './instructor.component.html',
   styleUrl: './instructor.component.css'
 })
 export class InstructorComponent {
   instructors: Instructor[]=[];
+  filteredInstructors: Instructor[] = [];
   message: string ='';
+  yogaSpecialities = Object.values(YogaSpeciality);
+  selectedSpecialities: string[] = []; //allow multiple select
 
   //constructor with injection
   constructor(private instructorService: InstructorsService){}
@@ -23,9 +29,31 @@ export class InstructorComponent {
     this.instructorService.getInstructors().subscribe({
       next: (value: Instructor[])=>{
         console.log('here');
-        this.instructors = value},
+        this.instructors = value;
+        this.filteredInstructors = value; //default to all
+      },
         complete:()=> console.log('instructor service finished'),
-        error: (message) =>this.message = message
-    })
-  }
+        error: (error) => {
+          console.error(error);
+          this.message = 'Failed to load instructors.';
+        },
+      });
+    }
+
+    filterInstructors(): void {
+      if (this.selectedSpecialities) {
+        this.filteredInstructors = this.instructors.filter((instructor) =>
+          this.selectedSpecialities.some((speciality) =>
+            instructor.yogaSpecialities.includes(speciality as YogaSpeciality)
+          )
+        );
+      } else {
+        this.filteredInstructors = this.instructors; // Show all if no filter is selected
+      }
+    }
+
+    resetFilter(): void {
+      this.selectedSpecialities = []; // Reset the selected filters
+      this.filteredInstructors = this.instructors; // Show all instructors
+    }
 }
